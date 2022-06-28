73956c65e7a7ab67ebc18e9ec06c8f7dcb1313df
Ketsarin Kayak <ketsarinkayak@Ketsarins-MacBook-Pro.local>
2022-06-27 21:59:53 -0700

    Express Simple Rest

--- server.js
+++ server.js
@@ -6,6 +6,46 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.all('/tea',(req,res,next) => {
+    res.statusCode= 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+app.get('/tes', (req,res) => {
+    res.end('Will send all the teasite to you');
+});
+app.post('./tea',(req,res) => {
+    res.end(`Will add the teasite: ${req.body.name} with description ${req.body.description}`)
+});
+app.put('/tea', (req,res) => {
+    res.status= 403;
+    res.end('PUT operation noy support on /tea');
+});
+app.delete('/tea', (req,res) => {
+    res.end('Delete all tea');
+});
+
+
+app.get('/tea/:teaId', (req, res) => {
+    res.end(`Will send details of the tea: ${req.params.teaId} to you`);
+});
+
+app.post('/tea/:teaId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /tea/${req.params.teaId}`);
+});
+
+app.put('/tea/:teaId', (req, res) => {
+    res.write(`Updating the tea: ${req.params.teaId}\n`);
+    res.end(`Will update the tea: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/tea/:teaId', (req, res) => {
+    res.end(`Deleting tea: ${req.params.teaId}`);
+});
 app.use(express.static(__dirname + '/public'));
 
 
